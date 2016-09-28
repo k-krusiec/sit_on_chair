bba6c59f891fd5af64f3e6046e578fc2bbd637f5
Kruchy <karol@krusiec.com>
2016-09-28 14:20:38 +0200

    dodane przenoszenie danych do pierwszego dropa

--- js/calc.js
+++ js/calc.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   var dropListArrows = document.querySelectorAll('.list_arrow');
-  var dropLists = document.querySelectorAll('.list_panel');
+  var dropDownLists = document.querySelectorAll('.drop_down_list');
   var dropItems = document.querySelectorAll('.list_panel li');
+  var summaryPanelLeft = document.querySelector('.panel_left');
+  var summaryPanelRight = document.querySelector('.panel_right');
+  var chairTitle = '';
+  var chairColor = '';
+  var chairPatern = '';
 
   for (var i = 0, len = dropListArrows.length; i < len; i++) {
     dropListArrows[i].addEventListener('click', function() {
@@ -20,6 +25,23 @@ document.addEventListener('DOMContentLoaded', function() {
       this.style.color = '';
       this.style.background = '';
     })
+    dropItems[i].addEventListener('click', function() {
+      console.log(this.innerText);
+      console.log(this.parentElement.parentElement.firstElementChild);
+      var dropText = this.parentElement.parentElement.firstElementChild;
+      dropText.style.color = 'black';
+      dropText.innerText = this.innerText;
+      this.parentElement.classList.remove('show');
+
+      chairTitle = dropDownLists[0].children[0].innerText;
+      chairColor = dropDownLists[1].children[0].innerText;
+      chairPatern = dropDownLists[0].children[0].innerText;
+
+      summaryPanelLeft.children[0].innerText = chairTitle;
+    })
+
   }
+
+
 
 })
